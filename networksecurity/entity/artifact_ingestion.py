ce2d2f4dd724900902775e688fc28dfeaf68e8c6
Gaurav Raj <graj174866@gmail.com>
2026-09-24 17:55:08 +0530

    Data Validation Completed

--- networksecurity/entity/artifact_ingestion.py
+++ networksecurity/entity/artifact_ingestion.py
@@ -1,12 +1,16 @@
+from dataclasses import dataclass
+
+@dataclass
 class DataIngestionArtifact:
+    trained_file_path:str
+    test_file_path:str
 
-    def __init__(self, trained_file_path: str, test_file_path: str):
-        self.trained_file_path = trained_file_path
-        self.test_file_path = test_file_path
 
-    def __repr__(self):
-        return (
-            f"DataIngestionArtifact("
-            f"trained_file_path={self.trained_file_path}, "
-            f"test_file_path={self.test_file_path})"
-        )
+@dataclass
+class DataValidationArtifact:
+    validation_status: bool
+    valid_train_file_path: str
+    valid_test_file_path: str
+    invalid_train_file_path: str
+    invalid_test_file_path: str
+    drift_report_file_path: str
